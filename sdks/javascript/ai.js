154ff787784a3ed40959cee39f0b0ee551488713
jonathanchaffer <jonathanchaffer@gmail.com>
2020-10-31 11:55:06 -0400

    Add isValidMove function to ai.js

--- sdks/javascript/ai.js
+++ sdks/javascript/ai.js
@@ -1,7 +1,14 @@
 function getMove(player, board) {
   // TODO: Determine valid moves
   // TODO: Calculate best move
-  return {column: 1};
+  return { column: 1 };
+}
+
+function isValidMove(move, board) {
+  // console.log("isValidMove");
+  if (move.column < 0 || move.column > 6) return false;
+  if (board[0][move.column] !== 0) return false;
+  return true;
 }
 
 function prepareResponse(move) {
@@ -10,4 +17,4 @@ function prepareResponse(move) {
   return response;
 }
 
-module.exports = { getMove, prepareResponse };
+module.exports = { getMove, isValidMove, prepareResponse };
--- sdks/javascript/test.js
+++ sdks/javascript/test.js
@@ -1,5 +1,28 @@
-const ai = require('./ai');
+const ai = require("./ai");
 
-test('returns a valid response', () => {
-  expect(ai.prepareResponse({column: 1})).toEqual(`{"column":1}\n`)
+test("returns a valid response", () => {
+  expect(ai.prepareResponse({ column: 1 })).toEqual(`{"column":1}\n`);
+});
+
+test("returns whether moves are valid", () => {
+  expect(
+    ai.isValidMove({ column: 0 }, [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+    ])
+  ).toBeTruthy();
+  expect(
+    ai.isValidMove({ column: 0 }, [
+      [2, 0, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0],
+      [2, 0, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0],
+      [2, 0, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0],
+    ])
+  ).toBeFalsy();
 });
